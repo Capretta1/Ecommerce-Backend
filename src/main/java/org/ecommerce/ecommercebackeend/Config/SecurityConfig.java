af68cda4eda3f177b8a70ecb099f90dd9ebc7e4d
Capretta1 <blessnutsua3@gmail.com>
2025-04-22 12:28:46 +0000

    y3nko koaa

--- src/main/java/org/ecommerce/ecommercebackeend/Config/SecurityConfig.java
+++ src/main/java/org/ecommerce/ecommercebackeend/Config/SecurityConfig.java
@@ -1,4 +1,45 @@
 package org.ecommerce.ecommercebackeend.Config;
 
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
+
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)throws Exception{
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+        ))
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(List.of("http"));
+                        config.setAllowedMethods(List.of("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(List.of("*"));
+                        config.setMaxAge(3600L
+                        );
+                        return config;
+                    }
+                }))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers())
+    }
 }
--- src/main/java/org/ecommerce/ecommercebackeend/Config/UserDetailsImp.java
+++ src/main/java/org/ecommerce/ecommercebackeend/Config/UserDetailsImp.java
@@ -1,4 +1,108 @@
 package org.ecommerce.ecommercebackeend.Config;
 
-public class UserDetails {
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.ecommerce.ecommercebackeend.Model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@NoArgsConstructor
+@Data
+public class UserDetailsImp implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    @Getter
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImp(Long id, String username, String email, String password,
+                           Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImp build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole().name()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImp(
+                user.getUserID(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+//   @Override
+//    public boolean equals(Object o) {
+//        if (this == o)
+//            return true;
+//        if (o == null || getClass() != o.getClass())
+//            return false;
+//        UserDetails user = (UserDetails) o;
+//        return Objects.equals(id, user.id);
+//    }
+
 }
+
